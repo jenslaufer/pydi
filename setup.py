a63dc91183167888a14b883dfbd7fb6efa2da9f6
jenslaufer <jenslaufer@gmail.com>
2020-05-07 10:34:02 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+setup(name='rltrader',
+      version='0.1.0',
+      author='Jens Laufer',
+      author_email='jenslaufer@gmail.com',
+      install_requires=[
+                        ],
+      packages=find_namespace_packages(where='src'),
+      package_dir={'': 'src'},
+      include_package_data=True
+      # package_data={'rltrader': ['rltrader/*.yml']}
+      # data_files=[('', ['src/rltrader/logging.yml'])]
+      )
---
+++ src/di/factory.py
@@ -0,0 +1,74 @@
+from .introspect import split_module_class
+from importlib import import_module
+import inspect
+import logging
+import types
+
+logger = logging.getLogger(__name__)
+
+def get_objects(d):
+    args = {}
+    module = ""
+    funct = ""
+    # collect constructor args (deep-first)...
+    for k, v in d.items():
+        if isinstance(v, dict):
+            # nestes objects
+            obj = get_objects(v)
+            args[k] = obj
+        else:
+            if k == 'name':
+                # defines the class to instantiate later
+                module, funct = split_module_class(v)
+            else:
+                # leaf object
+                args[k] = v
+
+    try:
+        if module + funct == "":
+            logger.debug('Node is a dict: %s', args)
+            return args
+
+        logger.debug("Resolving attribute %s.%s on %s", module, funct, d)
+        result = getattr(import_module(module), funct)
+
+        if isinstance(result, types.FunctionType):
+            logger.debug('%s is a function', result)
+        else:
+            logger.debug('Calling contructor %s with %s', result, args)
+            result = result(**args)
+
+        if isinstance(result, Factory):
+            result = result.get()
+
+    except Exception as e:
+        logging.exception(e)
+        raise
+
+    return result
+
+
+class Factory:
+
+    def __init__(self, target, args):
+        self.module, self.funct = split_module_class(target)
+        self.args = args
+
+    def get(self):
+        return getattr(import_module(self.module), self.funct)(**self.args)
+
+
+class ModuleFactory(Factory):
+    def __init__(self, target):
+        super(ModuleFactory, self).__init__(target, None)
+
+    def get(self):
+        return getattr(import_module(self.module), self.funct)
+
+
+class ListFactory(Factory):
+    def __init__(self, args):
+        self.args = args
+
+    def get(self):
+        return [self.args]
